2267decd46b95cfdc9b557a754ab2d00157dc208
Nick Gonner <nggonner@gmail.com>
2022-06-26 19:46:30 -0500

    Update our content.json generation to cover all the new randomized json files

--- app/ContentJSONHelper.py
+++ app/ContentJSONHelper.py
@@ -6,7 +6,16 @@ def generateContentJSONHelper():
     replacements = {
         "Data/Crops" : "assets/randomizedCrops.json", 
         "Data/ObjectInformation" : "assets/updatedObjectInformation.json",
-        "Data/Bundles" : "assets/randomizedBundles.json"
+        "Data/Bundles" : "assets/randomizedBundles.json",
+        "Data/FarmAnimals" : "assets/randomizedAnimals.json",
+        "Data/CookingRecipes" : "assets/randomizedCooking.json",
+        "Data/CraftingRecipes" : "assets/randomizedCrafting.json",
+        "Data/Fish" : "assets/randomizedFish.json",
+        "Data/fruitTrees" : "assets/randomizedFruitTrees.json",
+        "Data/Locations" : "assets/randomizedLocations.json",
+        "Data/mail" : "assets/randomizedMail.json",
+        "Data/Quests" : "assets/randomizedQuests.json",
+        "Data/TV/TipChannel" : "assets/randomizedTips.json"
         }
     for target, replacement in replacements.items():
         changeDict = { "Action" : "Load", "Target" : target, "FromFile": replacement }
